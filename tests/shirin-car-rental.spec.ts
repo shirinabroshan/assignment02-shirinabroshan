a736bbc262b0de2c97e8dbd3fc0cc60c092d7d03
shirinabroshan <shirinabroshan@gmail.com>
2024-09-24 09:49:18 +0200

    startar upp projeket

---
+++ tests/shirin-car-rental.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('shirin test car', () => {
+
+  test('test1 get order', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+
+  test('test 2 get castoumer', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+
+});
+
