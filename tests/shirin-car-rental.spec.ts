94877e872eb4d0b7fbf6c13603806960d21e3d70
shirinabroshan <shirinabroshan@gmail.com>
2024-09-24 12:58:59 +0200

    10 testfall request

--- tests/shirin-car-rental.spec.ts
+++ tests/shirin-car-rental.spec.ts
@@ -2,14 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test.describe('shirin test car', () => {
 
-  test('test1 get order', async ({ page }) => {
+  test('test1 get orders', async ({ page }) => {
     await page.goto('https://playwright.dev/');
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Playwright/);
   });
 
-  test('test 2 get castoumer', async ({ page }) => {
+  test('test 2 get castoumers', async ({ page }) => {
     await page.goto('https://playwright.dev/');
 
     // Expect a title "to contain" a substring.
